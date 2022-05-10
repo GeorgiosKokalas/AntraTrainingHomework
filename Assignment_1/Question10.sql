4d6b4b815518a2bb23204994ab35bf6f0672ce5f
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2022-05-10 18:02:48 -0500

    Create Question10.sql

---
+++ Assignment_1/Question10.sql
@@ -0,0 +1,21 @@
+/*
+10. Write a query that retrieves the columns Name and ListPrice
+from the Production.Product table. Your result set should look something like the following. Order the result set
+by the Name column. The products name should start with either 'A' or 'S'
+
+Name                                            ListPrice
+Adjustable Race                                   0,00
+All-Purpose Bike Stand                       159,00
+AWC Logo Cap                                      8,99
+Seat Lug                                          0,00
+Seat Post                                         0,00
+*/
+
+USE AdventureWorks2019
+GO
+
+SELECT TOP(5) "Name", "ListPrice"
+FROM Production.Product
+WHERE ("Name" LIKE 'A%'
+        OR "Name" LIKE 'S%')
+ORDER BY "Name" ASC
